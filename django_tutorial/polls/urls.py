379e8f8d1c42cc59a7e00f20217d7ea5e7e302d9
mbichoffe <bichoffe.marina@gmail.com>
2017-11-16 16:03:45 -0200

    new index view displaying the last 5 questions in the system

--- django_tutorial/polls/urls.py
+++ django_tutorial/polls/urls.py
@@ -3,6 +3,20 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    #ex: /polls/
+    # ex: /polls/
     url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name="results"),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+# note about regex
+# (?P<name>regex)
+# Captures the text matched by "regex" into the group "name".
+# The name can contain letters and numbers but must start with a letter.
+# the Named capturing group 'question_id' matches a set of chars in the range
+# of 0-9 (indicated by the square brackets and -)
+
 ]
--- django_tutorial/polls/views.py
+++ django_tutorial/polls/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from .models import Question
+# Create your views here.
+
+
 
 def index(request):
-    return HttpResponse("Hello, world, You're at the polls index.")
+    """Display the latest 5 poll questions in the system 
+        according to publication date
+    """
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
 
 def detail(request, question_id):
-    "displays a question text, with no results but with a form to vote."
+    """Displays a question text, with no results but with a form to vote."""
     return HttpResponse("You're looking at question {}".format(question_id))
 
+
 def results(request, question_id):
-    "displays results for a particular question."
+    """Displays results for a particular question."""
     response = "You're looking at the results of question {}".format(question_id)
     return HttpResponse(response)
 
-def vote(request,question_id):
-    "handles voting for a particular choice in a particular question."
+
+def vote(request, question_id):
+    """Handles voting for a particular choice in a particular question."""
     return HttpResponse("You\'re voting on question {}".format(question_id))
-    
