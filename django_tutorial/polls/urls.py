be41f80641a173b793e6a9095af1e05489c9e16b
mbichoffe <bichoffe.marina@gmail.com>
2017-11-20 11:19:49 -0200

    converted app to use generic views system

--- django_tutorial/polls/urls.py
+++ django_tutorial/polls/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.details, name='details'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailsView.as_view(), name='details'),
     # ex: /polls/5/results
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name="results"),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name="results"),
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 
--- django_tutorial/polls/views.py
+++ django_tutorial/polls/views.py
@@ -3,29 +3,24 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from .models import Choice, Question
+from django.db.models import F
 # Create your views here.
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def index(request):
-    """Display the latest 5 poll questions in the system 
-        according to publication date
-    """
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class DetailsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
 
-
-def details(request, question_id):
-    """Displays a question text, with no results but with a form to vote."""
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/details.html', {'question': question})
-
-
-def results(request, question_id):
-    """Displays results for a particular question."""
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
@@ -40,7 +35,21 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
+        """
+        Although reporter.stories_filed = F('stories_filed') + 1 looks like
+        a normal Python assignment of value to an instance attribute, in fact
+        it’s an SQL construct describing an operation on the database.
+        When Django encounters an instance of F(), it overrides the standard
+        Python operators to create an encapsulated SQL expression; in this case, 
+        one which instructs the database to increment the database field
+        represented by reporter.stories_filed.
+
+        Whatever value is or was on reporter.stories_filed, Python never gets
+        to know about it - it is dealt with entirely by the database.
+        All Python does, through Django’s F() class, is create the SQL
+        syntax to refer to the field and describe the operation
+        """
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing with
         # POST data. This prevents data from being posted twice if a
