8ce27c94c3834e7df66961df2ddc0a74c95f737c
mbichoffe <bichoffe.marina@gmail.com>
2017-11-21 09:24:27 -0200

    added fonts and title to index page

--- django_tutorial/polls/admin.py
+++ django_tutorial/polls/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
 
-admin.site.register(Question)
+
+admin.site.register(Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
+"""
+You’ll follow this pattern – create a model admin class, then pass it as the 
+second argument to admin.site.register() – any time you need to change the 
+admin options for a model.
+"""
