36af50c012a63cf27d92654a6f8d37175476db72
mbichoffe <bichoffe.marina@gmail.com>
2017-11-23 09:38:39 -0200

    customized questions admin page

--- django_tutorial/polls/admin.py
+++ django_tutorial/polls/admin.py
@@ -3,13 +3,21 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-
-admin.site.register(Choice)
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'],
+                              'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
 
 admin.site.register(Question, QuestionAdmin)
+
 """
 You’ll follow this pattern – create a model admin class, then pass it as the 
 second argument to admin.site.register() – any time you need to change the 
